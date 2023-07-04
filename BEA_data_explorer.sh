57734cf18c827c0af9f5dd773ec17ac10f5a4f47
jadesrochers <jadesrochers@gmail.com>
2023-07-04 17:12:06 -0400

    Updates to get data for an overhauled nextjs census data site

--- BEA_data_explorer.sh
+++ BEA_data_explorer.sh
@@ -1,5 +1,5 @@
 # Get the major top level data sets
-curl -X GET -o dl_bea_datasetlist.json -L "http://apps.bea.gov/api/data?UserID=74B6144A-CFBF-48F9-9A6E-F50213F7FA39&method=getDataList&ResultFormat=JSON"
+curl -X GET -o dl_bea_datasetlist.json -L "http://apps.bea.gov/api/data?UserID=74B6144A-CFBF-48F9-9A6E-F50213F7FA39&method=getDataSetList&ResultFormat=JSON"
 # Get the parameters that can be passed to one of those top level sets
 curl -X GET -o dl_bea_dataset_parameters.json -L "http://apps.bea.gov/api/data?UserID=74B6144A-CFBF-48F9-9A6E-F50213F7FA39&method=getParameterList&datasetname=Regional&ResultFormat=JSON"
 
@@ -10,11 +10,50 @@ curl -X GET -o dl_bea_dataset_Regional_LineCodes.json -L "http://apps.bea.gov/ap
 # Get the specific tables available:
 curl -X GET -o dl_bea_dataset_Regional_TableNames.json -L "http://apps.bea.gov/api/data?UserID=74B6144A-CFBF-48F9-9A6E-F50213F7FA39&method=GetParameterValues&datasetname=Regional&ParameterName=TableName&ResultFormat=JSON"
 
+
+##### Short list of abbreviation meanings 
+# Becuase there are a ton and it is hard to work out, their documentation
+# is hard to find
+
+##### IMPORTANT: Table and LineCode
+# The Table is defined by the leading Letters that you will see repeated a lot (CAINC for example)
+# and the trailing value (1, 30, 5N, 5S) is the LineCode
+
+### Should start with indicator if this is state or county or something else
+# S - State
+# C - County
+#
+### Then the frequency
+# A - Annual
+# Q - Quarterly 
+# M - Monthly
+# SQA - Quarterly, seasonally adjusted
+# QNSA - Quarterly, non-seasonally adjusted
+#
+### Then an abbreviation of what the set is about 
+# INC - Income
+# 
+
+### Full Table Name - 
+### The codes above are modified by an id - this can be number, letters
+# There are different tables for: (County, Annual, Income)
+# CAINC1
+# CAINC30
+# CAINC4
+# CAINC5N
+# CAINC5S
+### Which contain differing, but also often overlapping data
+
+## Determining Line codes - 
+curl -X GET -o dl_bea_linecodes_CAINC1.json -L "http://apps.bea.gov/api/data?UserID=74B6144A-CFBF-48F9-9A6E-F50213F7FA39&method=GetParameterValuesFiltered&datasetname=Regional&TargetParameter=LineCode&TableName=CAINC1&ResultFormat=JSON"
+
 # Get nice pretty print of the result
 jq -C < dl_bea_dataset_parameter_args.json
 
+
 ## Now get the data (only one field at a time, disappointing):
-curl -X GET -o dl_bea_dataset_CAINC1.json -L "http://apps.bea.gov/api/data?UserID=74B6144A-CFBF-48F9-9A6E-F50213F7FA39&method=GetData&datasetname=Regional&GeoFips=NY&TableName=CAINC1&LineCode=1,2,3&Year=All&ResultFormat=JSON"
+curl -X GET -o dl_bea_dataset_CAINC1_2.json -L "http://apps.bea.gov/api/data?UserID=74B6144A-CFBF-48F9-9A6E-F50213F7FA39&method=GetData&datasetname=Regional&GeoFips=NY&TableName=CAINC1&LineCode=2&Year=All&ResultFormat=JSON"
+
 ## Get a different field - checkout out what is feasible to get
 curl -X GET -o dl_bea_dataset_CAINC1_92.json -L "http://apps.bea.gov/api/data?UserID=74B6144A-CFBF-48F9-9A6E-F50213F7FA39&method=GetData&datasetname=Regional&GeoFips=NY&TableName=CAINC1&LineCode=92&Year=All&ResultFormat=JSON"
 
--- BEA_download_and_transform.sh
+++ BEA_download_and_transform.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
 
-Dataset=$1
-# Dataset=CAINC1
-Region=$2
-# Region=NY
+# Dataset=$1
+Dataset=CAINC1
+
+# Use "COUNTY" for all counties
+# Region=$2
+Region=COUNTY
+
+# Linecode
+# LineCode=$3
+LineCode=2
+
+# Can specify year range
 Years=All
+
+# JSON or XML, thats it
 Format=JSON
+
+# My token
 UserToken=74B6144A-CFBF-48F9-9A6E-F50213F7FA39
+
+
+#### RATE LIMIT - 
+# It is 100 requests per min or 100MB of data per min. If you go over, bad,
+# because you get blocked for an hour
 
 # Get available linecodes for a dataset
 # This may (or may not) be specific to the Regional dataset
 get_linecodes() {
     tablename="$1"
     local filename="tmp_BEA_linecodes.json"
-    curl -X GET -o "$filename" -L "http://apps.bea.gov/api/data?UserID=${UserToken}&method=GetParameterValues&datasetname=Regional&ParameterName=LineCode&ResultFormat=${Format}" 
+    curl -X GET -o "$filename" -L "https://apps.bea.gov/api/data?UserID=${UserToken}&method=GetParameterValues&datasetname=Regional&ParameterName=LineCode&ResultFormat=${Format}" 
+    # curl -X GET -o "$filename" -L "http://apps.bea.gov/api/data?UserID=${UserToken}&method=GetParameterValuesFiltered&datasetname=Regional&TargetParameter=LineCode&TableName=${Dataset}&ResultFormat=${Format}"
+    # printf "linecode data: %s\n" "$(jq < tmp_BEA_linecodes.json)"
     linecodes=$(jq < tmp_BEA_linecodes.json | grep -C 1 -i "$tablename" | grep -i key | sed 's/"//g' | sed -r 's/[^0-9]*([0-9]+).*/\1/' | uniq)
     rm "$filename"
     printf '%s\n' "$linecodes"
@@ -25,9 +44,9 @@ get_data_and_metadata() {
     linecode="$2"
     local dl_filename="tmp_BEA_datameta_${tablename}_${linecode}.json"
     local csv_filename="dl_BEA_${tablename}_fulltable.csv"
-    curl -X GET -o "${dl_filename}" -L "http://apps.bea.gov/api/data?UserID=74B6144A-CFBF-48F9-9A6E-F50213F7FA39&method=GetData&datasetname=Regional&GeoFips=${Region}&TableName=${tablename}&LineCode=${linecode}&Year=${Years}&ResultFormat=${Format}"
+    curl -X GET -o "${dl_filename}" -L "https://apps.bea.gov/api/data?UserID=74B6144A-CFBF-48F9-9A6E-F50213F7FA39&method=GetData&datasetname=Regional&GeoFips=${Region}&TableName=${tablename}&LineCode=${linecode}&Year=${Years}&ResultFormat=${Format}"
     jq -r '["GeoFips", "GeoName", "TimePeriod", .BEAAPI.Results.Data[0].Code], (.BEAAPI.Results.Data | sort_by(.GeoFips,.TimePeriod)[] | [.GeoFips,.GeoName,.TimePeriod,.DataValue]) | @csv' < "$dl_filename" > "$csv_filename"
-    rm "$dl_filename"
+    # rm "$dl_filename"
     printf '%s' "$csv_filename"
     set +x
 }
@@ -39,7 +58,7 @@ append_more_data() {
     local dl_filename="tmp_BEA_data_${tablename}_${linecode}.json"
     local csv_filename="dl_BEA_${tablename}_datatable.csv"
     local new_filename="dl_BEA_${tablename}_fulltable_${linecode}.csv"
-    curl -X GET -o "${dl_filename}" -L "http://apps.bea.gov/api/data?UserID=${UserToken}&method=GetData&datasetname=Regional&GeoFips=${Region}&TableName=${tablename}&LineCode=${linecode}&Year=${Years}&ResultFormat=${Format}"
+    curl -X GET -o "${dl_filename}" -L "https://apps.bea.gov/api/data?UserID=${UserToken}&method=GetData&datasetname=Regional&GeoFips=${Region}&TableName=${tablename}&LineCode=${linecode}&Year=${Years}&ResultFormat=${Format}"
     jq -r '[.BEAAPI.Results.Data[0].Code], (.BEAAPI.Results.Data | sort_by(.GeoFips,.TimePeriod)[] | [.DataValue]) | @csv' < "$dl_filename" > "$csv_filename"
     paste -d ',' "$base_csv" "$csv_filename" > "$new_filename"
     rm "$dl_filename" "$csv_filename" "$base_csv"
@@ -47,16 +66,19 @@ append_more_data() {
 }
 
 
+## Don't use this unless you know it won't go over the size limit
+# LineCodes=($(get_linecodes "$Dataset"))
+# n=0
+# basefilename=''
+# for linecode in ${LineCodes[@]}; do
+#     sleep 60;
+#     if [[ $n -eq 0 ]]; then
+#         basefilename=$(get_data_and_metadata "$Dataset" "$linecode")
+# 	printf "Downloaded initial data to: %s" "$basefilename"
+#     else
+#         basefilename=$(append_more_data "$Dataset" "$linecode" "$basefilename")
+#         n=$((n+1))
+#     fi
+# done
 
-LineCodes=($(get_linecodes "$Dataset"))
-n=0
-basefilename=''
-for linecode in ${LineCodes[@]}: do
-    local basefilename
-    if n -eq 0; then
-        basefilename=$(get_data_and_metadata "$Dataset" "$linecode")
-    else
-        basefilename=$(append_more_data "$Dataset" "$linecode" "$basefilename")
-        n=((n+1))
-done
-
+basefilename=$(get_data_and_metadata "$Dataset" "$linecode")
