bef97c1438b79f71570a9ed7c7fa131d5e28ebf1
jadesrochers <jadesrochers@gmail.com>
2023-07-07 20:59:26 -0400

    Set the download to compile full table again

--- BEA_download_and_transform.sh
+++ BEA_download_and_transform.sh
@@ -39,16 +39,14 @@ get_linecodes() {
 }
 
 get_data_and_metadata() {
-    set -x
     tablename="$1"
     linecode="$2"
     local dl_filename="tmp_BEA_datameta_${tablename}_${linecode}.json"
-    local csv_filename="dl_BEA_${tablename}_fulltable.csv"
+    local csv_filename="dl_BEA_${tablename}_${linecode}.csv"
     curl -X GET -o "${dl_filename}" -L "https://apps.bea.gov/api/data?UserID=74B6144A-CFBF-48F9-9A6E-F50213F7FA39&method=GetData&datasetname=Regional&GeoFips=${Region}&TableName=${tablename}&LineCode=${linecode}&Year=${Years}&ResultFormat=${Format}"
     jq -r '["GeoFips", "GeoName", "TimePeriod", .BEAAPI.Results.Data[0].Code], (.BEAAPI.Results.Data | sort_by(.GeoFips,.TimePeriod)[] | [.GeoFips,.GeoName,.TimePeriod,.DataValue]) | @csv' < "$dl_filename" > "$csv_filename"
     # rm "$dl_filename"
     printf '%s' "$csv_filename"
-    set +x
 }
 
 append_more_data() {
@@ -56,31 +54,40 @@ append_more_data() {
     linecode="$2"
     base_csv="$3"
     local dl_filename="tmp_BEA_data_${tablename}_${linecode}.json"
-    local csv_filename="dl_BEA_${tablename}_datatable.csv"
+    local csv_filename="tmp_dl_BEA_${tablename}_${linecode}.csv"
     local new_filename="dl_BEA_${tablename}_fulltable_${linecode}.csv"
     curl -X GET -o "${dl_filename}" -L "https://apps.bea.gov/api/data?UserID=${UserToken}&method=GetData&datasetname=Regional&GeoFips=${Region}&TableName=${tablename}&LineCode=${linecode}&Year=${Years}&ResultFormat=${Format}"
     jq -r '[.BEAAPI.Results.Data[0].Code], (.BEAAPI.Results.Data | sort_by(.GeoFips,.TimePeriod)[] | [.DataValue]) | @csv' < "$dl_filename" > "$csv_filename"
     paste -d ',' "$base_csv" "$csv_filename" > "$new_filename"
     rm "$dl_filename" "$csv_filename" "$base_csv"
-    printf "%s" "$new_filename" 
+    printf '%s' "$new_filename" 
 }
+
+
+## Python one liner to get csv into json in case you want to have
+## Whole csv files and then pull them into json
+# cat my.csv | python -c 'import csv, json, sys; print(json.dumps([dict(r) for r in csv.DictReader(sys.stdin)]))'
 
 
 ### !!! Don't use this unless you know it won't go over the size limit
 ## Size limit is 100 requests or 100MB per min
 
-# LineCodes=($(get_linecodes "$Dataset"))
-# n=0
-# basefilename=''
-# for linecode in ${LineCodes[@]}; do
-#     sleep 60;
-#     if [[ $n -eq 0 ]]; then
-#         basefilename=$(get_data_and_metadata "$Dataset" "$linecode")
-# 	printf "Downloaded initial data to: %s" "$basefilename"
-#     else
-#         basefilename=$(append_more_data "$Dataset" "$linecode" "$basefilename")
-#         n=$((n+1))
-#     fi
-# done
+LineCodes=($(get_linecodes "$Dataset"))
+n=0
+basefilename=''
+for linecode in ${LineCodes[@]}; do
+    if [[ $n -eq 0 ]]; then
+        basefilename=$(get_data_and_metadata "$Dataset" "$linecode")
+	printf 'Downloaded initial data to: %s\n\n' "$basefilename"
+        n=$((n+1))
+    else
+	printf 'basefilename: %s\n\n' "$basefilename"
+        basefilename=$(append_more_data "$Dataset" "$linecode" "$basefilename")
+	printf 'Appended data for linecode: %s\nto: %s\n\n' "$linecode" "$basefilename"
+    fi
+    sleep 60;
+done
+mv "$basefilename" "dl_BEA_${Dataset}_fulltable_complete.csv"
 
-basefilename=$(get_data_and_metadata "$Dataset" "$LineCode")
+
+# basefilename=$(get_data_and_metadata "$Dataset" "$LineCode")
